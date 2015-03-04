5416333b3948c9fb9a6536dfb8989aad14b2e799
Jimmy Thigpen <jmthigpen@me.com>
2015-03-04 14:04:22 -0500

    fixedStuff

--- app/routes/recipes.js
+++ app/routes/recipes.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return Ember.RSVP.hash({
-      recipes: this.store.findAll('recipe'),
-    });
+      return this.store.findAll('recipe');
   },
-  setupController: function(controller, model){
-   controller.set('model', model.recipes);
-
- }
 });
