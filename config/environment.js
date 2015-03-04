babce821896757806006b2536605a91c378ddeca
Jimmy Thigpen <jmthigpen@me.com>
2015-03-03 23:10:41 -0500

    recipes template

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
       'script-src': "'self'",
       'font-src': "'self'",
       'connect-src': "'self' https://api.parse.com",
-      'img-src': "'self'",
+      'img-src': "'self' http://files.parsetfss.com",
       'style-src': "'self'",
       'media-src': "'self'"
     }
