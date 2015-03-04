04ae4f5818f6bbdec86487ad7e75a1c27866b783
Erik Eggers <em.eggers@gmail.com>
2015-03-04 16:49:25 -0500

    updated saveRecipe action

--- app/controllers/recipes/new.js
+++ app/controllers/recipes/new.js
@@ -1,8 +1,47 @@
 import Ember from 'ember';
 
 
-
 export default Ember.Controller.extend({
   recipeType: ["Breakfast", "Lunch", "Dinner", "Deserts", "Snacks"],
-  ingredientsUnit: ["cups", "ounces", "teaspoons", "tablespoons", "pounds"]
+  ingredientsUnit: ["cups", "ounces", "teaspoons", "tablespoons", "pounds"],
+
+  recipe: {},
+
+  actions: {
+    saveRecipe: function(){
+      var data = {
+      "name": this.get('recipe.name'),
+      "author": this.get('recipe.author'),
+      "type": this.get('recipe.type'),
+      "prepTime": this.get('recipe.prepTime'),
+      "cookTime": this.get('recipe.cookTime'),
+      "cookTemp": this.get('recipe.cookTemp'),
+      "notes": this.get('recipe.notes'),
+      "yieldNumber": this.get('recipe.yieldNumber'),
+      "yieldName": this.get('recipe.yieldName'),
+
+
+
+
+      "steps":[{
+          "stepNumber": 1,
+          "ingredients":
+            [{
+              "amount": this.get('ingredients.amount'),
+              "unit": this.get('ingredients.unit'),
+              "name": this.get('ingredients.name'),
+            }],
+              "directions": this.get('recipe.directions'),
+          }],
+
+      // "amount": this.get('ingredients.amount'),
+      // "unit": this.get('ingredients.unit'),
+      // "ingredients": this.get('ingredients.name'),
+      // "stepNumber": 1
+    };
+    console.log(data.stepNumber);
+    console.log(data);
+    }
+  }
+
 });
