829faa016692884601ce57e9774ac20bdb0063d1
Jimmy Thigpen <jmthigpen@me.com>
2015-03-05 16:12:33 -0500

    adding a recipe console log working completely

--- app/controllers/recipes/new.js
+++ app/controllers/recipes/new.js
@@ -16,32 +16,10 @@ export default Ember.Controller.extend({
   actions: {
     saveRecipe: function(){
       var data = this.getProperties('name', "author", "type", "prepTime", "cookTime", "cookTemp", "notes", "yieldNumber", "yieldName", "steps");
-    //   var data = {
-    //   "name": this.get('recipe.name'),
-    //   "author": this.get('recipe.author'),
-    //   "type": this.get('recipe.type'),
-    //   "prepTime": this.get('recipe.prepTime'),
-    //   "cookTime": this.get('recipe.cookTime'),
-    //   "cookTemp": this.get('recipe.cookTemp'),
-    //   "notes": this.get('recipe.notes'),
-    //   "yieldNumber": this.get('recipe.yieldNumber'),
-    //   "yieldName": this.get('recipe.yieldName'),
-    //   "steps":[{
-    //       "stepNumber": 1,
-    //
-    //         "directions": this.get('recipe.directions'),
-    //       }],
-    // };
-    console.log(JSON.stringify(data));
+      console.log(JSON.stringify(data));
   },
 
       addStep: function(){
-        // var ingredients = [{
-        //   "amount": this.get('ingredient.amount'),
-        //   "unit": this.get('ingredient.unit'),
-        //   "name": this.get('ingredient.name'),
-        // }];
-        // console.log(ingredients);
         this.get('steps').addObject({ingredients: [{}]});
   },
 }
