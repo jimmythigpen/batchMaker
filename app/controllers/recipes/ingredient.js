49e3d3dd1af5514b78c457fad3cdeed4d2c8b147
Jasper Furniss <jasperfurniss@gmail.com>
2015-03-06 16:08:04 -0500

    working-adjust

--- app/controllers/recipes/ingredient.js
+++ app/controllers/recipes/ingredient.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['recipes/recipe'],
+  scaledAmount: function(){
+    return Number(this.get('model.amount')) * Number(this.get('controllers.recipes/recipe.scaleMult'));
+  }.property('model.amount', 'controllers.recipes/recipe.scaleMult')
 });
