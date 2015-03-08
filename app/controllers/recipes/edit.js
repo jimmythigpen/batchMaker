c98d964107a158d7accfe7f4d834f83c7da096fb
Erik Eggers <em.eggers@gmail.com>
2015-03-08 18:30:49 -0400

    delete works

--- app/controllers/recipes/edit.js
+++ app/controllers/recipes/edit.js
@@ -13,6 +13,16 @@ export default Ember.Controller.extend({
         data: JSON.stringify(model),
         contentType: 'application/json'
       });
+    },
+    delete: function(){
+      var model = this.get('model');
+      var _this = this;
+      Ember.$.ajax({
+        url: "https://api.parse.com/1/classes/Recipes/" + model.id,
+        type: "DELETE",
+      }).done(function(){
+        _this.transitionToRoute('recipes.index');
+      });
     }
   },
 });
