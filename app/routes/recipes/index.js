eed7ba71890119695de5983f789a543731ec1700
Jimmy Thigpen <jmthigpen@me.com>
2015-03-09 09:17:35 -0400

    removed console logs and adjust button

--- app/routes/recipes/index.js
+++ app/routes/recipes/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-      console.log('findAll router is running');
       return this.store.findAll('recipe');
   },
 });
